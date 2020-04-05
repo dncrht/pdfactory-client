571fa213a7d99b2bcb86d28a6ddf390941c989a4
Dani <daniel.cr.ht@gmail.com>
2020-04-05 19:03:53 +0200

    Gem initialization

---
+++ lib/pdfactory/client.rb
@@ -0,0 +1,9 @@
+require "pdfactory/client/version"
+
+module PDFactory
+  class Client
+    class Error < StandardError; end
+
+    def kk;puts 1;end
+  end
+end
---
+++ spec/pdfactory/client_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe PDFactory::Client do
+  it "has a version number" do
+    expect(PDFactory::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
